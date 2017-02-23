26731bcf50ca2878081cd881e03518854b98d03e
David Heller <heller_d@molgen.mpg.de>
2017-02-23 14:48:07 +0100

    increase version to 1.0.0; add GPL license file

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools.extension import Extension
 long_description = """RNA-binding proteins (RBPs) play a vital role in the post-transcriptional control of RNAs. They are known to recognize RNA molecules by their nucleotide sequence as well as their three-dimensional structure. ssHMM is an RNA motif finder that combines a hidden Markov model (HMM) with Gibbs sampling to learn the joint sequence and structure binding preferences of RBPs from high-throughput RNA-binding experiments, such as CLIP-Seq. The model can be visualized as an intuitive graph illustrating the interplay between RNA sequence and structure."""
 
 setup(name='sshmm',
-      version='1.0.0b4',
+      version='1.0.0',
       description='A sequence-structure hidden Markov model for the analysis of RNA-binding protein data.',
       long_description=long_description,
-      url='https://github.molgen.mpg.de/heller/ssHMM',
+      url=['https://github.molgen.mpg.de/heller/ssHMM', 'http://sshmm.readthedocs.io'],
       author='David Heller, Annalisa Marsico',
       author_email='heller_d@molgen.mpg.de',
       license='GPLv3',
       classifiers=[
-      'Development Status :: 4 - Beta',
+      'Development Status :: 5 - Production/Stable',
       'Environment :: Console',
       'Intended Audience :: Science/Research',
       'Topic :: Scientific/Engineering :: Bio-Informatics',
